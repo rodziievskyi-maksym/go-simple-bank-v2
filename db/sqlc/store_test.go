fb330af5b98f5e5dbacd0cca5d561b6af6d73be6
Maksym Rodziievskyi <rodziyevskydev@gmail.com>
2025-08-30 22:08:02 +0300

    small update on test

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"testing"
 
-	"github.com/rodziievskyi-maksym/go-simple-bank-v2/util"
 	"github.com/stretchr/testify/require"
 )
 
@@ -14,11 +13,11 @@ func TestTransferTx(t *testing.T) {
 	account1 := createRandomAccount(t)
 	account2 := createRandomAccount(t)
 
-	randomAmount := util.RandomMoney()
+	amount := int64(10)
 	arg := TransferTxParams{
 		FromAccountID: account1.ID,
 		ToAccountID:   account2.ID,
-		Amount:        randomAmount,
+		Amount:        amount,
 	}
 
 	concurrentTransactions := 5
@@ -46,7 +45,7 @@ func TestTransferTx(t *testing.T) {
 		require.NotEmpty(t, transfer)
 		require.Equal(t, account1.ID, transfer.FromAccountID)
 		require.Equal(t, account2.ID, transfer.ToAccountID)
-		require.Equal(t, randomAmount, transfer.Amount)
+		require.Equal(t, amount, transfer.Amount)
 		require.NotZero(t, transfer.ID)
 		require.NotZero(t, transfer.CreatedAt)
 
@@ -57,7 +56,7 @@ func TestTransferTx(t *testing.T) {
 		fromEntry := result.FromEntry
 		require.NotEmpty(t, fromEntry)
 		require.Equal(t, account1.ID, fromEntry.AccountID)
-		require.Equal(t, -randomAmount, fromEntry.Amount)
+		require.Equal(t, -amount, fromEntry.Amount)
 		require.NotZero(t, fromEntry.ID)
 		require.NotZero(t, fromEntry.CreatedAt)
 
@@ -67,7 +66,7 @@ func TestTransferTx(t *testing.T) {
 		toEntry := result.ToEntry
 		require.NotEmpty(t, toEntry)
 		require.Equal(t, account2.ID, toEntry.AccountID)
-		require.Equal(t, randomAmount, toEntry.Amount)
+		require.Equal(t, amount, toEntry.Amount)
 		require.NotZero(t, toEntry.ID)
 		require.NotZero(t, toEntry.CreatedAt)
 
@@ -75,5 +74,20 @@ func TestTransferTx(t *testing.T) {
 		require.NoError(t, err)
 
 		//TODO: check account balance
+		fromAccount := result.FromAccount
+		require.NotEmpty(t, fromAccount)
+		require.Equal(t, account1.ID, fromAccount.ID)
+
+		toAccount := result.ToAccount
+		require.NotEmpty(t, toAccount)
+		require.Equal(t, account2.ID, toAccount.ID)
+
+		//diff
+		diff1 := account1.Balance - fromAccount.Balance // account 1 balance (50) - updated Balance Account 1  = was 50 now 40
+		diff2 := toAccount.Balance - account2.Balance   //  updated balance of Account 2 (toAccount) - account 2 balance before transaction = 40 now 50
+		require.Equal(t, diff1, diff2)
+		require.True(t, diff1 > 0)
+		//TODO: what's going on here?
+		require.True(t, diff1%amount == 0)
 	}
 }
